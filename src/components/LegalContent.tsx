4d64a738822df6984ab4d120fe490e392ef889aa
Luis Carlos Cabezas Castro <contacto@cencomer.com>
2025-06-26 10:48:34 -0500

    Avance

--- src/components/LegalContent.tsx
+++ src/components/LegalContent.tsx
@@ -10,8 +10,8 @@ interface LegalContentProps {
 export default function LegalContent({ title, content }: LegalContentProps) {
   const { darkMode } = useTheme();
   return (
-    <div className={`max-w-6xl mx-auto p-8 md:p-12 shadow-md rounded-b-xl mt-4 ${darkMode ? 'bg-gray-900' : 'bg-white'}`}>
-      <h1 className={`text-3xl font-bold mb-8 ${darkMode ? 'text-white' : 'text-gray-900'}`}>{title}</h1>
+    <div className={`max-w-6xl mx-auto p-8 md:p-12 shadow-md rounded-xl mt-4 ${darkMode ? 'bg-gray-800' : 'bg-gray-50'}`}>
+      <h1 className={`text-3xl font-bold mb-8 border-b pb-2 ${darkMode ? 'text-white' : 'text-gray-900'}`}>{title}</h1>
       <div className={`prose prose-lg ${darkMode ? 'dark:prose-invert text-gray-300' : 'text-gray-700'}`}>
         {content.map((paragraph, index) => (
           <p key={index} className="mb-4">
